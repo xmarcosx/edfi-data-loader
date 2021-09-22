dfb7ae59956b1830bd7f4077d4eb664c322d2b1c
Marcos Alcozer <marcos@alcozer.dev>
2021-09-22 01:21:10 +0000

    Init

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, render_template
+
+# pylint: disable=C0103
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    message = "It's running!"
+
+    return message
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
